b5c0f0c7c2cc03df2c11f51e369681440ebe2ead
rubcuadra <rubcuadra@gmail.com>
2017-01-17 08:02:23 -0600

    script to run

---
+++ run_1er_unitarias.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+cd googletest/googletest/make
+make
+cp gtest_main.a ../../../1erParcial/Unitarias
+echo 'recursive/'
+g++ -isystem ../../googletest/googletest/include/ -pthread recursive/first.cc  recursive/first_unit.cc gtest_main.a  -o prueba
+./prueba
+echo 'almacen/'
+g++ -isystem ../../googletest/googletest/include/ -pthread almacen/main.cc almacen/almacen_test.cc  gtest_main.a  -o prueba
+./prueba
+echo 'parametrized/'
+g++ -isystem ../../googletest/googletest/include/ -pthread  parametrized/parametrized.cc   gtest_main.a  -o prueba
+g++ -isystem ../../googletest/googletest/include/ -pthread  parametrized/parametrized.cc   gtest_main.a  -o prueba
