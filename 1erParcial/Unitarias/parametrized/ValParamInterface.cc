17a7905811e5f52ff393ff26da604f30cbf08fdf
rubcuadra <rubcuadra@gmail.com>
2017-01-20 09:15:36 -0600

    test

---
+++ 1erParcial/Unitarias/parametrized/ValParamInterface.cc
@@ -0,0 +1,46 @@
+#include "gtest/gtest.h"
+#include "../typed/hierarchie.h"
+
+typedef Base* factoryMethod();
+
+Base* instance1()
+{
+	return new Child1;
+}
+
+Base* instance2()
+{
+	return new Child2;
+}
+
+template<int parameter>
+Base* instanceWithConstParam()
+{
+	return new Child3(parameter);
+}
+
+class Fixture : public testing::TestWithParam<factoryMethod * >
+{
+protected:
+	Base * instance2Test;
+public:
+	void SetUp()
+	{
+		instance2Test = ( *GetParam() ) () ;
+
+	}
+	void TearDown()
+	{
+		delete instance2Test;
+		instance2Test = NULL;
+
+	}
+};
+
+TEST_P(Fixture,testNameParams)
+{
+	this->instance2Test->doSomething();
+}
+
+INSTANTIATE_TEST_CASE_P(CaseName,Fixture,testing::Values( &instance1 , &instance2, &instanceWithConstParam<8> )) ;
+
