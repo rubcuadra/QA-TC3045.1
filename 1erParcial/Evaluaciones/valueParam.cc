17ca53b2f059f67d2bb066bb51f99609d8ef418b
rubcuadra <rubcuadra@gmail.com>
2017-01-24 09:40:31 -0600

    finished test

--- 1erParcial/Evaluaciones/valueParam.cc
+++ 1erParcial/Evaluaciones/valueParam.cc
@@ -39,17 +39,75 @@ TEST_P(FixValParam,burbujas_asc)
 	int *array = &arr[0]; //Array necesario por que asi fue creado el .h
 
 	sorter->burbuja( array , arr.size() , sorter->asc );
-	//	arr.at(0) < arr.at(1) < arr.at(2)
+	for (int i = 0; i < arr.size()-1; ++i)
+		EXPECT_LE( arr.at(i) , arr.at(i+1));	// <=
 }
 
 TEST_P(FixValParam,burbujas_desc)
 {
 	std::vector<int> arr = GetParam();
-	int *array = &arr[0]; //Array necesario por que asi fue creado el .h
+	int *array = &arr[0]; 
 	sorter->burbuja( array , arr.size() , sorter->desc );
-	//	arr.at(0) > arr.at(1) > arr.at(2)
+	for (int i = 0; i < arr.size()-1; ++i)
+		EXPECT_GE(  arr.at(i) , arr.at(i+1));  // >=
 }
 
+TEST_P(FixValParam,insercion_asc)
+{
+	std::vector<int> arr = GetParam();
+	int *array = &arr[0]; //Array necesario por que asi fue creado el .h
+
+	sorter->insercion( array , arr.size() , sorter->asc );
+	for (int i = 0; i < arr.size()-1; ++i)
+		EXPECT_LE( arr.at(i) , arr.at(i+1));	// <=
+}
+
+TEST_P(FixValParam,insercion_desc)
+{
+	std::vector<int> arr = GetParam();
+	int *array = &arr[0]; 
+	sorter->insercion( array , arr.size() , sorter->desc );
+	for (int i = 0; i < arr.size()-1; ++i)
+		EXPECT_GE(  arr.at(i) , arr.at(i+1));  // >=
+}
+
+TEST_P(FixValParam,seleccion_asc)
+{
+	std::vector<int> arr = GetParam();
+	int *array = &arr[0]; //Array necesario por que asi fue creado el .h
+
+	sorter->seleccion( array , arr.size() , sorter->asc );
+	for (int i = 0; i < arr.size()-1; ++i)
+		EXPECT_LE( arr.at(i) , arr.at(i+1));	// <=
+}
+
+TEST_P(FixValParam,seleccion_desc)
+{
+	std::vector<int> arr = GetParam();
+	int *array = &arr[0]; 
+	sorter->seleccion( array , arr.size() , sorter->desc );
+	for (int i = 0; i < arr.size()-1; ++i)
+		EXPECT_GE(  arr.at(i) , arr.at(i+1));  // >=
+}
+
+TEST_P(FixValParam,quicksort_asc)
+{
+	std::vector<int> arr = GetParam();
+	int *array = &arr[0]; //Array necesario por que asi fue creado el .h
+
+	sorter->quicksort( array , 0,arr.size() , sorter->asc );
+	for (int i = 0; i < arr.size()-1; ++i)
+		EXPECT_LE( arr.at(i) , arr.at(i+1));	// <=
+}
+
+TEST_P(FixValParam,quicksort_desc)
+{
+	std::vector<int> arr = GetParam();
+	int *array = &arr[0]; 
+	sorter->quicksort( array , 0,arr.size() , sorter->desc );
+	for (int i = 0; i < arr.size()-1; ++i)
+		EXPECT_GE(  arr.at(i) , arr.at(i+1));  // >=
+}
 
 std::vector<int> v1 = {7, 5, 16, 8};
 std::vector<int> v2 = {10, 3, 22, 112};
